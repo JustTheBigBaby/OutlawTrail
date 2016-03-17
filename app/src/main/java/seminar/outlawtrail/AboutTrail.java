6fa98c3cbe9cfae30783b57389e65c7638af5cf1
Aaron Hughes <ahughes1729@gmail.com>
2016-03-16 20:37:15 -0500

    Initial commit

---
+++ app/src/main/java/seminar/outlawtrail/AboutTrail.java
@@ -0,0 +1,32 @@
+package seminar.outlawtrail;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AboutTrail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_trail);
+
+    }
+
+
+
+    public void go_home(View view) {
+        Intent home = new Intent(this, MainActivity.class);
+        startActivity(home);
+    }
+
+    public void go_to_map(View view) {
+        Intent map = new Intent(this, MapsActivity.class);
+        startActivity(map);
+    }
+
+}
---
+++ app/src/main/java/seminar/outlawtrail/MainActivity.java
@@ -0,0 +1,28 @@
+package seminar.outlawtrail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void go_to_about(View view) {
+        Intent about = new Intent(this, AboutTrail.class);
+        startActivity(about);
+    }
+
+    public void go_to_map(View view) {
+        Intent map = new Intent(this, MapsActivity.class);
+        startActivity(map);
+    }
+
+
+}
