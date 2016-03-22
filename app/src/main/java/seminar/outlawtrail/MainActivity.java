fce0962ee828936c89675067c0af5136166f09ec
Aaron Hughes <ahughes1729@gmail.com>
2016-03-21 19:38:57 -0500

    Added a web view functionality to the WebMap page, restructured some activities, now when user click the "map" button it opens the map in either a browser or Google Maps.

--- app/src/main/java/seminar/outlawtrail/MainActivity.java
+++ app/src/main/java/seminar/outlawtrail/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void go_to_map(View view) {
-        Intent map = new Intent(this, MapsActivity.class);
+        Intent map = new Intent(this, WebMap.class);
         startActivity(map);
     }
 
--- app/src/main/java/seminar/outlawtrail/WebMap.java
+++ app/src/main/java/seminar/outlawtrail/WebMap.java
@@ -1,6 +1,7 @@
 package seminar.outlawtrail;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,25 +9,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class AboutTrail extends AppCompatActivity {
+public class WebMap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_trail);
-
+        setContentView(R.layout.activity_web_map);
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/d/u/0/viewer?mid=z5cNDCbZgqkg.kReB3k9EGYyI"));
+        startActivity(i);
     }
 
-
+    public void go_to_about(View view) {
+        Intent about = new Intent(this, AboutTrail.class);
+        startActivity(about);
+    }
 
     public void go_home(View view) {
         Intent home = new Intent(this, MainActivity.class);
         startActivity(home);
-    }
-
-    public void go_to_map(View view) {
-        Intent map = new Intent(this, WebMap.class);
-        startActivity(map);
     }
 
 }
