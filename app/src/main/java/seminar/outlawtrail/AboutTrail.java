1f119e0c39f2c5bb07b817513862eb6eb3a71c9c
Aaron Hughes <ahughes1729@gmail.com>
2016-03-23 16:54:44 -0500

    Test4

--- app/src/main/java/seminar/outlawtrail/AboutTrail.java
+++ app/src/main/java/seminar/outlawtrail/AboutTrail.java
@@ -19,6 +19,7 @@ public class AboutTrail extends AppCompatActivity {
 
 //Test again
 //Figuring it out
+    //send help
     public void go_home(View view) {
         Intent home = new Intent(this, MainActivity.class);
         startActivity(home);
